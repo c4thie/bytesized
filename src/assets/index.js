8f286cf4708f64e57b682bb9e486f1269bbad613
Jasmine Xiong <jasminexiongx@gmail.com>
2023-09-16 19:33:02 -0400

    fix button color

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,9 @@
 import boba1 from "./boba_getstarted.png";
 import boba2 from "./boba_results.png";
+import close from "./icon.svg";
 import icon from "./bobaicon.svg";
 import loader from "./bobaloader.gif";
 import pinkTwinkle from "./pink-twinkle-shiny.gif";
 import singleloader from "./boba_loader.gif";
-import close from "./icon.svg";
 
-export { close, boba1, boba2, icon, loader, pinkTwinkle, singleloader };
+export { boba1, boba2, close, icon, loader, pinkTwinkle, singleloader };
