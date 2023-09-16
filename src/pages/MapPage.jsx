b602778ebed20102a1ff4e2d711f113674c75a35
Jasmine Xiong <jasminexiongx@gmail.com>
2023-09-16 14:17:52 -0400

    add map markers

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
 import Loading from "../components/Loading";
 
 const containerStyle = {
@@ -8,8 +8,8 @@ const containerStyle = {
 };
 
 const center = {
-  lat: 43.4723,
-  lng: -80.5449,
+  lat: 43.473046551051176,
+  lng: -80.53836586627632,
 };
 
 const MapPage = () => {
@@ -22,19 +22,33 @@ const MapPage = () => {
     <div>
       byte sized map
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={16}>
-        {/* STC */}
-        <Marker position={{ lat: 43.470837, lng: -80.54411 }} />
-        {/* SLC */}
-        <Marker
-          position={{ lat: 43.47185265218329, lng: -80.54534359583154 }}
-        />
-        {/* DC */}
-        <Marker
+        <MarkerF
           position={{ lat: 43.47291357044051, lng: -80.54269479945117 }}
+          label="Chatime"
         />
-        {/* E7 */}
-        <Marker
-          position={{ lat: 43.47304812731901, lng: -80.53952274900837 }}
+        <MarkerF
+          position={{ lat: 43.47207286948884, lng: -80.53905491794353 }}
+          label="Sweet Dreams"
+        />
+        <MarkerF
+          position={{ lat: 43.47177310692343, lng: -80.538539933834237 }}
+          label="The Alley"
+        />
+        <MarkerF
+          position={{ lat: 43.47623215976741, lng: -80.53861950549259 }}
+          label="Gong Cha"
+        />
+        <MarkerF
+          position={{ lat: 43.47301112552648, lng: -80.53501529298342 }}
+          label="Machi Machi"
+        />
+        <MarkerF
+          position={{ lat: 43.47284174311075, lng: -80.53388417305472 }}
+          label="CoCo Fresh Tea & Juice"
+        />
+        <MarkerF
+          position={{ lat: 43.47555180479398, lng: -80.53557187580549 }}
+          label="Now Tea"
         />
       </GoogleMap>
     </div>
